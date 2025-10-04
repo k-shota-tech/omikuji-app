91f771d3d1af4ade7129c4088ee9c908c29c2cd4
k-shota <kshokamekame.1020@gmail.com>
2025-10-04 16:56:33 +0900

    大凶にした

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("btn");
 const result = document.getElementById("result");
 
 btn.addEventListener("click", function() {
-  const fortunes = ["大吉✨", "中吉😊", "小吉🍀", "凶💦"];
+  const fortunes = ["大吉✨", "中吉😊", "小吉🍀", "超大大凶💦"];
   const random = Math.floor(Math.random() * fortunes.length);
   result.textContent = fortunes[random];
 });
